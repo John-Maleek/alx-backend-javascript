2620c34bc82a183faed8adf884b6e4dd40434eac
John Maleek <jmzoran01@gmail.com>
2024-04-25 01:35:57 +0100

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((val) => {
-      console.log(val[0].body, val[1].firstName, val[1].lastName);
+  Promise.all([uploadPhoto(), createUser(), Promise.reject()])
+    .then((values) => {
+      let obj = {};
+      for (const item of values) {
+        obj = { ...obj, ...item };
+      }
+      console.log(obj.body, obj.firstName, obj.lastName);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.error('Signup system offline'));
 }
+
+handleProfileSignup();
