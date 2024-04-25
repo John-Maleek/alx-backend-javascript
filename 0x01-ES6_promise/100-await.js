e89aea7a34927862ddccaf5c784c554a668d4db0
John Maleek <jmzoran01@gmail.com>
2024-04-25 02:13:45 +0100

    advanced tasks

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const res1 = await uploadPhoto();
   const res2 = await createUser();
-  return { phote: res1, user: res2 };
+  return { photo: res1, user: res2 };
 }
