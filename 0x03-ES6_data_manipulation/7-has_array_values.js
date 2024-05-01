2f95432fe0c9de9358011843fa79b2af85c9d941
John Maleek <jmzoran01@gmail.com>
2024-05-01 03:41:41 +0100

    corrections

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 export default function hasValuesFromArray(set, array) {
-  if (Array.isArray(array) && set instanceof Set) {
+  if (Array.isArray(array) && array.length > 0 && set instanceof Set) {
     const arraySet = new Set(array);
     return set.isSupersetOf(arraySet);
   }
