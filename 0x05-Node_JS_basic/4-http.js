ffbe12b85f9f3c1dad98dbc069cb89d55862b992
John Maleek <jmzoran01@gmail.com>
2024-06-19 01:43:21 +0100

    first commit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
@@ -9,6 +8,6 @@ const app = http.createServer((req, res) => {
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
   console.log('Listening to requests on port 1245');
 });
