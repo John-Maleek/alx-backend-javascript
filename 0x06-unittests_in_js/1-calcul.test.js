47fb719ff15baccb15c21ef0272d0f20b1b361e6
John Maleek <jmzoran01@gmail.com>
2024-06-21 00:47:07 +0100

    second commit

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -9,11 +9,20 @@ describe('calculateNumber', function () {
   it('expects 1 + 3 = 4', function () {
     assert.equal(calculateNumber('SUM', 1, 3), 4);
   });
+  it('expects -1 + -3 = -4', function () {
+    assert.equal(calculateNumber('SUM', -1, -3), -4);
+  });
   it('expects 1.4 + 4.5 = -4', function () {
     assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
+  it('expects -4 + 4.5 = 1', function () {
+    assert.equal(calculateNumber('SUBTRACT', -4, 4.5), -9);
+  });
   it('expects 1.4 / 4.5 = 0.2', function () {
     assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+  it('expects -120 / 4 = -30', function () {
+    assert.equal(calculateNumber('DIVIDE', -120, 4), -30);
   });
   it('expects Error', function () {
     assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,11 +7,20 @@ describe('calculates number', () => {
   it('expects 1 + 3 = 4', () => {
     expect(calculateNumber('SUM', 1, 3)).to.equal(4);
   });
+  it('expects -1 + -3 = -4', function () {
+    expect(calculateNumber('SUM', -1, -3)).to.equal(-4);
+  });
   it('expects 1.4 + 4.5 = -4', () => {
     expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
+  it('expects -4 + 4.5 = 1', function () {
+    expect(calculateNumber('SUBTRACT', -4, 4.5)).to.equal(-9);
+  });
   it('expects 1.4 / 4.5 = 0.2', () => {
     expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('expects -120 / 4 = -30', function () {
+    expect(calculateNumber('DIVIDE', -120, 4)).to.equal(-30);
   });
   it('expects Error', () => {
     expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
