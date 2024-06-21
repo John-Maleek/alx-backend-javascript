d0a3f2048430a95ce9c9b6c17ffe046c69998d7a
John Maleek <jmzoran01@gmail.com>
2024-06-21 01:10:43 +0100

    task 1 correction

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -7,24 +7,24 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function () {
   it('expects 1 + 3 = 4', function () {
-    assert.equal(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
   });
   it('expects -1 + -3 = -4', function () {
-    assert.equal(calculateNumber('SUM', -1, -3), -4);
+    assert.strictEqual(calculateNumber('SUM', -1, -3), -4);
   });
   it('expects 1.4 + 4.5 = -4', function () {
-    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
   it('expects -4 + 4.5 = 1', function () {
-    assert.equal(calculateNumber('SUBTRACT', -4, 4.5), -9);
+    assert.strictEqual(calculateNumber('SUBTRACT', -4, 4.5), -9);
   });
   it('expects 1.4 / 4.5 = 0.2', function () {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
   it('expects -120 / 4 = -30', function () {
-    assert.equal(calculateNumber('DIVIDE', -120, 4), -30);
+    assert.strictEqual(calculateNumber('DIVIDE', -120, 4), -30);
   });
   it('expects Error', function () {
-    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 });
