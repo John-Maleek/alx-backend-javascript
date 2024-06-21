1cef06a1ae210edce81e5ab2a9e69afd5dc4e6c3
John Maleek <jmzoran01@gmail.com>
2024-06-21 01:15:23 +0100

    task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/lowercase-name */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
@@ -7,12 +8,15 @@ const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('Testing...', () => {
+    const calculateNumberSpy = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
 
     calculateNumberSpy.restore();
+    consoleLogSpy.restore();
   });
 });
